c79f19e6e4c7fc047db72766e71fc98faf74f1e2
Nilesh <nilesh@haachico.com>
2022-12-18 11:28:45 +0530

    add messsage for negative or empty input in isTriangle

--- istriangle.js
+++ istriangle.js
@@ -7,15 +7,30 @@ function calcSumOfAngles(angle1, angle2, angle3) {
   return sumOfAngles;
 }
 function isTriangle() {
-  const sumOfAngles = calcSumOfAngles(
-    Number(anglesInput[0].value),
-    Number(anglesInput[1].value),
-    Number(anglesInput[2].value)
-  );
-  if (sumOfAngles === 180) {
-    output.innerText = `Yay! It is a traingle!`;
+  const angleOneInput = Number(anglesInput[0].value);
+  const angleTwoInput = Number(anglesInput[1].value);
+  const angleThreeInput = Number(anglesInput[2].value);
+
+  if (
+    angleOneInput < 0 ||
+    angleOneInput === 0 ||
+    angleTwoInput < 0 ||
+    angleTwoInput === 0 ||
+    angleThreeInput < 0 ||
+    angleThreeInput === 0
+  ) {
+    output.innerText = `Please enter valid positive angle.`;
   } else {
-    output.innerText = "It is not a triangle.";
+    const sumOfAngles = calcSumOfAngles(
+      angleOneInput,
+      angleTwoInput,
+      angleThreeInput
+    );
+    if (sumOfAngles === 180) {
+      output.innerText = `Yay! It is a traingle!`;
+    } else {
+      output.innerText = "It is not a triangle.";
+    }
   }
 }
 
